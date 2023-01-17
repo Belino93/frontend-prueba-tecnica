39d6b6674e4b73dadd2a7e1e5dfc9f3480ade8a9
belino93 <abelmadrid93@gmail.com>
2023-01-17 21:28:49 +0100

    add create new contract

---
+++ src/services/apiCalls.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+const baseUrl = "https://backend-prueba-tecnica-production.up.railway.app/";
+
+export const getContracts = async () => {
+  try {
+    const contractsData = await axios.get(baseUrl + "listcontracts");
+    return contractsData;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getLocalidad = async (codigoP) => {
+  try {
+    if (codigoP === "") {
+      return;
+    }
+    const body = { cp: codigoP };
+    const localidad = await axios.post(baseUrl + "getlocalidad", body);
+
+    if (localidad?.data?.message) {
+      return localidad.data.message;
+    }
+
+    return localidad.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const createContract = async (contractData) => {
+  try {
+    const newContract = await axios.post(baseUrl + "addcontract", contractData);
+
+    return newContract;
+  } catch (error) {
+    return error;
+  }
+};
