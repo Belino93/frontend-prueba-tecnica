b818a2d3124f6bc2bdc6991fd95bb8f468769501
belino93 <abelmadrid93@gmail.com>
2023-01-18 10:42:40 +0100

    delete button, change borrar document state

--- src/containers/List/List.jsx
+++ src/containers/List/List.jsx
@@ -3,26 +3,44 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useState, useEffect } from "react";
-import { getContracts } from "../../services/apiCalls";
+import { getContracts, checkForDeleteContract } from "../../services/apiCalls";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
 import ModalCustom from "../../components/Modal";
+import Modal from "react-bootstrap/Modal";
+import './List.css'
 
 function List() {
   const [contracts, setContracts] = useState([]);
   const [show, setShow] = useState(false);
   const [contractId, setContractId] = useState("");
+  const [contractClicked, setContractClicked] = useState({});
   const [isUpdate, setIsUpdate] = useState(false);
   const [isNew, setIsNew] = useState(false);
   const showModal = () => setShow(true);
+  const [refresh, setRefresh] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
 
   useEffect(() => {
     getContracts()
       .then((res) => {
-        setContracts(res.data);
+        const filteredContracts = res.data.filter(
+          (contract) => contract.borrar === false
+        );
+        setContracts(filteredContracts);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [refresh]);
+
+  const deleteHandler = () => {
+    checkForDeleteContract(contractClicked?._id)
+      .then((res) => {
+        setRefresh(!refresh);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -32,8 +50,9 @@ function List() {
           <Row>
             <Col className="col-3 mb-1">
               <button
+                className="buttons bg-primary bg-opacity-50 "
                 onClick={() => {
-                  setIsUpdate(false)
+                  setIsUpdate(false);
                   setIsNew(true);
                   showModal();
                 }}
@@ -61,10 +80,10 @@ function List() {
                       <td>
                         <div className="d-flex justify-content-center gap-1">
                           <button
-                            className=" bg-warning"
+                            className="buttons bg-warning"
                             onClick={() => {
                               setIsUpdate(true);
-                              setIsNew(false)
+                              setIsNew(false);
                               setContractId(contract._id);
                               showModal();
                             }}
@@ -72,7 +91,13 @@ function List() {
                             <FontAwesomeIcon icon={faPencil} />
                             <span className="mx-1">Editar</span>
                           </button>
-                          <button className=" bg-danger">
+                          <button
+                            className="bg-danger buttons"
+                            onClick={() => {
+                              setDeleteModal(true);
+                              setContractClicked(contract);
+                            }}
+                          >
                             <FontAwesomeIcon icon={faTrash} />
                             <span className="mx-1">Borrar</span>
                           </button>
@@ -94,9 +119,34 @@ function List() {
             contractId={contractId}
             isNew={isNew}
             setIsNew={setIsNew}
-
+            setRefresh={setRefresh}
+            refresh={refresh}
           />
         )}
+        <Modal show={deleteModal} onHide={() => setDeleteModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Borrar contrato</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className=" text-center">
+            ¿Estas seguro que quieres marcar como borrado el contrato con el
+            documento{" "}
+            <span className="fw-bold">{contractClicked?.documento}?</span>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className="buttons" onClick={() => setDeleteModal(false)}>
+              Cerrar
+            </button>
+            <button
+              className="bg-danger buttons"
+              onClick={() => {
+                deleteHandler();
+                setDeleteModal(false);
+              }}
+            >
+              Borrar
+            </button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </>
   );
